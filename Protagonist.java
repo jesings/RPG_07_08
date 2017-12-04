34030c78f9f2fe7988588ef76ebceea84e851251
Jonathan Singer <jsinger@stuy.edu>
2017-12-04 18:48:11 -0500

    added RPGV1

---
+++ Protagonist.java
@@ -0,0 +1,23 @@
+//By Jonathan Singer and Matthew Ming
+public class Protagonist extends Character{
+    private String name;
+    public Protagonist(String myName){
+     name = myName;
+     HP = 125;
+     strength = 100;
+     defense = 40;
+     attack = 0.4;
+    }
+    public String getName(){
+        return name;
+    }
+    public void specialize(){
+            defense = 5;
+            attack = .6;
+    }
+    public void normalize(){
+            defense = 10;
+            attack = .4;
+    }
+ }
+ 
