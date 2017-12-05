9e1b1133b1f70c1db2b94d3c59ec2b3ef273567b
Jonathan Singer <jsinger@stuy.edu>
2017-12-04 19:20:15 -0500

    readded root files

---
+++ Character.java
@@ -0,0 +1,25 @@
+public class Character{
+    protected int HP;
+    protected int strength;
+    protected int defense;
+    protected double attack;
+    public boolean isAlive(){
+        return HP > 0;
+    }
+    public int getDefense(){
+        return defense;
+    }
+    public void lowerHP(int damage){
+        HP -= damage;
+    }
+    public int attack(Character victim){
+        int damageDealing = (int) (strength * attack - victim.getDefense());
+        if(damageDealing >0){
+            victim.lowerHP(damageDealing);
+            return damageDealing;
+        }
+        else{ 
+            return 0;
+        }
+    }
+}
---
+++ Monster.java
@@ -0,0 +1,10 @@
+//Made by Matthew Ming and Jonathan Singer
+public class Monster extends Character{
+    public Monster(){
+     HP=(int)Math.random()*100+100;
+        strength=(int)Math.random()*45+30;
+     defense=(int)Math.random()*20+5;
+     attack=Math.random()*2.5;
+    }
+ }
+ 
