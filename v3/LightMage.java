34c87d4182cbfb6b3caf596e0ab3974cc2564f2d
Jonathan Singer <jsinger@stuy.edu>
2017-12-13 21:42:57 -0500

    resolved issues

--- v3/LightMage.java
+++ v3/LightMage.java
@@ -6,10 +6,6 @@ public class LightMage extends Mage{
         spellBook[2] = "Magic Missile";
     }
     public String toString(){
-<<<<<<< HEAD
-        return super.toString() + ",a light magician";
-=======
         return super.toString() + " that specializes in Light Magic";
->>>>>>> 1ed8f86d29d7c70999d0098eef769d381876f9a5
     }
 }
